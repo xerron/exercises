aa0a4163939a19ed98b7d5518899fca0ee8819de
E. Manuel Cerrón Angeles <xerron.angels@gmail.com>
2020-05-27 06:13:20 -0500

    add FareEstimatorTest

--- tests/FareEstimatorTest.php
+++ tests/FareEstimatorTest.php
@@ -19,6 +19,18 @@ final class FareEstimatorTest extends TestCase
             new FareEstimator($rideTime, $rideDistance, $costPerMinute, $costPerMile)
         );
     }
+    public function testCalculate()
+    {
+        $rideTime = 30;
+        $rideDistance = 7;
+        $costPerMinute = [0.2, 0.35, 0.4, 0.45];
+        $costPerMile = [1.1, 1.8, 2.3, 3.5];
+
+        $this->assertEquals(
+            [13.7, 23.1, 28.1, 38],
+            (new FareEstimator($rideTime, $rideDistance, $costPerMinute, $costPerMile))->calculate()
+        );
+    }
 }
 
 
